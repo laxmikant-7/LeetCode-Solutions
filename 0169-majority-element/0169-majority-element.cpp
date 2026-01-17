6ea53af4da063e15e8f932b5026ac6ce1f2eea70
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-01-17 11:22:53 +0530

    Time: 0 ms (100%), Space: 28.1 MB (62.21%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,17 +3,15 @@ public:
     int majorityElement(vector<int>& nums) {
         int n=nums.size();
         if(n==1 || n==2) return nums[0];
-        sort(nums.begin(),nums.end());
-        int count=1;
-        for(int i=1;i<n;i++){
-            if(nums[i]==nums[i-1]){
-                count++;
-            }
+        int count=0;
+        int element=nums[0];
+        for(int i=0;i<n;i++){
+            if(nums[i]==element) count++;
             else{
-                if(count>(n/2)) return nums[i-1];
-                count=1;
+                count--;
+                if(count==0) element=nums[i+1];
             }
         }
-        return nums[n-1];
+        return element;
     }
 };
