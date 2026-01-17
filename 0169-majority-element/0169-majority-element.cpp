2e08b3e9aa38026186d3162c3f9125fb0b686507
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-01-17 11:24:26 +0530

    Time: 0 ms (100%), Space: 28.3 MB (7.53%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -4,13 +4,14 @@ public:
         int n=nums.size();
         if(n==1 || n==2) return nums[0];
         int count=0;
-        int element=nums[0];
+        int element;
         for(int i=0;i<n;i++){
-            if(nums[i]==element) count++;
-            else{
-                count--;
-                if(count==0) element=nums[i+1];
+            if(count==0){
+                element=nums[i];
+                count=1;
             }
+            else if(nums[i]==element) count++;
+            else count--;
         }
         return element;
     }
