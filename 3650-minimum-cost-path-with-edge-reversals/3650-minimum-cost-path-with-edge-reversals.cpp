bbb6523bac8faa1981fcdd475685dac6fd23410f
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-01-27 23:23:59 +0530

    Time: 282 ms (53.84%), Space: 295.4 MB (60.58%) - LeetHub

---
+++ 3650-minimum-cost-path-with-edge-reversals/3650-minimum-cost-path-with-edge-reversals.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int minCost(int n, vector<vector<int>>& edges) {
+
+        vector<vector<pair<int,int>>> adj(n);
+
+        for (auto &e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+
+            // normal edge
+            adj[u].push_back({v, w});
+
+            // reversed edge using switch (cost = 2*w)
+            adj[v].push_back({u, 2 * w});
+        }
+
+        const long long INF = 1e18;
+        vector<long long> dist(n, INF);
+
+        priority_queue<
+            pair<long long,int>,
+            vector<pair<long long,int>>,
+            greater<>
+        > pq;
+
+        dist[0] = 0;
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+            auto [cost, u] = pq.top();
+            pq.pop();
+
+            if (cost > dist[u]) continue;
+
+            for (auto &[v, w] : adj[u]) {
+                if (dist[v] > cost + w) {
+                    dist[v] = cost + w;
+                    pq.push({dist[v], v});
+                }
+            }
+        }
+
+        return dist[n-1] == INF ? -1 : (int)dist[n-1];
+    }
+};
