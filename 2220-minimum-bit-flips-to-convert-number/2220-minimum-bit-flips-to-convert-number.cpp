e3fe6dd3540931c258ef63aebc0b69feddc7e8b0
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-03-05 06:50:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (26.53%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int flips=0;
+        while(start!=0 || goal!=0){
+            if((start&1)!=(goal&1)) flips++;
+            start=start>>1;
+            goal=goal>>1;
+        }
+        return flips;
+    }
+};
