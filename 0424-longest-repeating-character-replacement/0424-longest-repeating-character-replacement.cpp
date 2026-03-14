e0262c1088dffb1c9e1265c47d484e37b6fd7877
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-03-14 19:47:25 +0530

    Time: 7 ms (43.43%), Space: 10.7 MB (92.68%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        int i=0;
+        int maxlen=0;
+        int maxfreq=0;
+        vector<int> freq(26,0);
+        for(int j=0;j<n;j++){
+            freq[s[j]-'A']++;
+            maxfreq=max(maxfreq,freq[s[j]-'A']);
+
+            while((j-i+1)-maxfreq>k){
+                freq[s[i]-'A']--;
+                maxfreq=0;
+                for(int k=0;k<26;k++) maxfreq=max(maxfreq,freq[k]);
+                i++;
+            }
+
+            maxlen=max(maxlen,j-i+1);
+        }  
+        return maxlen;
+    }
+};
