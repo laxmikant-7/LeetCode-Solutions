c4db94c36bfc2a4045cfd706ccc59cd8606b4b0b
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-03-14 10:51:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    void solve(string curr,vector<string> &temp,int n){
+    void solve(string curr,string &ans,int n,int &count,int k){
         if(curr.size()==n){
-            temp.push_back(curr);
+            count++;
+            if(count==k) ans=curr;
             return;
         }
         for(char ch='a';ch<='c';ch++){
             if(curr.size()!=0 && curr.back()==ch) continue;
             curr+=ch;
-            solve(curr,temp,n);
+            solve(curr,ans,n,count,k);
             curr.pop_back();
         }
     }
     string getHappyString(int n, int k) {
-        int no_strings=3*(1<<(n-1));
-        if(no_strings<k) return "";
-        vector<string> temp;
-        solve("",temp,n);
-        return temp[k-1];
+        string ans="";
+        int count=0;
+        solve("",ans,n,count,k);
+        return ans;
     }
 };
