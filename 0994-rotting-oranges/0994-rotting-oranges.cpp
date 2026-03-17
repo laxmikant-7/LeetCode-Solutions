aecad37eb12f41dd04d61f485638edf408944703
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-03-17 23:20:16 +0530

    Time: 1 ms (37.31%), Space: 17 MB (41.43%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int freshCount=0;
+        vector<vector<int>> visited(n,vector<int>(m));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                    visited[i][j]=2;
+                }
+                if(grid[i][j]==1) freshCount++;
+            }
+        }
+
+        int tm=0;
+        int drow[]={-1,0,1,0};
+        int dcol[]={0,1,0,-1};
+        int cnt=0;
+        while(!q.empty()){
+            int r=q.front().first.first;
+            int c=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            tm=max(t,tm);
+            for(int i=0;i<4;i++){
+                int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && visited[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+                    visited[nrow][ncol]=2;
+                    q.push({{nrow,ncol},t+1});
+                    cnt++;
+                }
+            }
+        }
+        return (freshCount!=cnt)?-1:tm;
+    }
+};
