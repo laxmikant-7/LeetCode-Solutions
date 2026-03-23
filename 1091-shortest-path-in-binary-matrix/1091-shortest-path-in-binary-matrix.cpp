d1b1af23c8c185881210b55c01e166e325d2aed1
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-03-23 18:34:03 +0530

    Time: 72 ms (14.38%), Space: 36.7 MB (14.06%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,36 +2,36 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n=grid.size();
-        if(n==1 && grid[0][0]==0) return 1;
-        if(grid[0][0]==1) return -1;
-        if(grid[n-1][n-1]==1) return -1;
-        int dx[]={-1,-1,0,1,1,1,0,-1};
-        int dy[]={0,1,1,1,0,-1,-1,-1};
-        vector<vector<int>> vis(n,vector<int>(n,INT_MAX));
-     
-        queue<pair<int,pair<int,int>>> q;
-        q.push({1,{0,0}});
-        while(!q.empty()){
-            int currdist=q.front().first;
-            int x=q.front().second.first;
-            int y=q.front().second.second;
-            q.pop();
-            for(int k=0;k<8;k++){
-                int nx=x+dx[k];
-                int ny=y+dy[k];
-                if(nx>=0 && nx<n && ny>=0 && ny<n){
-                    if(grid[nx][ny]==1){
-                        continue;
-                    }
-                    else{
-                    if(vis[nx][ny]>currdist+1){
-                        vis[nx][ny]=currdist+1;
-                        q.push({currdist+1,{nx,ny}});
+        
+        if(grid[0][0]==1 || grid[n-1][n-1]==1) return -1;
+        vector<vector<int>> dist(n,vector<int>(n,INT_MAX));
+        int dx[] = {-1, -1, -1,  0, 0,  1, 1, 1};
+        int dy[] = {-1,  0,  1, -1, 1, -1, 0, 1};
+        set<pair<int,pair<int,int>>> st;
+        dist[0][0]=1;
+        grid[0][0]=1;
+        st.insert({1,{0,0}});
+
+        while(!st.empty()){
+            auto it=*(st.begin());
+            int dis=it.first;
+            int x=it.second.first;
+            int y=it.second.second;
+            st.erase(it);
+            if(x==n-1 && y==n-1) return dis;
+            for(int i=0;i<8;i++){
+                int nx=x+dx[i];
+                int ny=y+dy[i];
+                int ndis=dist[x][y]+1;
+                if(nx>=0 && nx<n && ny>=0 && ny<n && grid[nx][ny]==0){
+                    if(dist[nx][ny]>ndis){
+                        dist[nx][ny]=ndis;
+                        st.insert({ndis,{nx,ny}});
                     }
                 }
-                }
-            }
+            }  
         }
-        return (vis[n-1][n-1]==INT_MAX)?-1:vis[n-1][n-1];
+        if(dist[n-1][n-1]==INT_MAX) return -1;
+        return dist[n-1][n-1];
     }
 };
