4277ff19a2118424066b18142d22eb5a57bbd948
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-01 18:16:11 +0530

    Time: 3 ms (9.34%), Space: 9.4 MB (50.59%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    int find(int m,int n,int i,int j,vector<vector<int>> &dp){
-        if(i>=m || j>=n) return 0;
-        if(i==m-1 && j==n-1) return 1;
-
-        if(dp[i][j]!=-1) return dp[i][j];
-
-        int r=find(m,n,i,j+1,dp);
-        int l=find(m,n,i+1,j,dp);
-
-        return dp[i][j]=r+l;
+    bool valid(int i,int j,int m,int n){
+        if(i<0 || j<0 || i>=m || j>=n) return false;
+        return true;
     }
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        int i=0;
-        int j=0;
-        return find(m,n,i,j,dp);
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        dp[0][0]=1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else{
+                    int up=(valid(i-1,j,m,n))?dp[i-1][j]:0;
+                    int left=(valid(i,j-1,m,n))?dp[i][j-1]:0;
+                    dp[i][j]=up+left;
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
