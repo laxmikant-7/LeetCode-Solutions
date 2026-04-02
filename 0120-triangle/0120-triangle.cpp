a33b549b9839a3c9153e4dd9eb559cbfab2d0ffd
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-02 22:24:02 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -2,23 +2,21 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& tri) {
         int m=tri.size();
-        vector<vector<int>> dp;
-        for(int i=0;i<m;i++){
+        vector<int> dp=tri[0];
+        for(int i=1;i<m;i++){
             vector<int> temp;
             for(int j=0;j<tri[i].size();j++){
-                if(i==0 && j==0) temp.push_back(tri[0][0]);
-                else{
+        
                     int up=INT_MAX;
                     int left=INT_MAX;
-                    if(i>0 && j < dp[i-1].size()) up=dp[i-1][j];
-                    if(i>0 && j>0) left=dp[i-1][j-1];
+                    if(j<dp.size()) up=dp[j];
+                    if(j!=0) left=dp[j-1];
                     int mini=tri[i][j]+min(up,left);
                     temp.push_back(mini);
-                }
             }
-            dp.push_back(temp);
+            dp=temp;
         }
-        auto it=min_element(dp[m-1].begin(),dp[m-1].end());
+        auto it=min_element(dp.begin(),dp.end());
         return *it;
     }
 };
