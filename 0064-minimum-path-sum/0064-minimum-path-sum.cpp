b2633320439a98c4fd67fafc10b7d1ea65bd1326
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-02 20:06:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -25,20 +25,20 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<int> dp(n,INT_MAX);
         for(int i=0;i<m;i++){
+            vector<int> temp(n,-1);
             for(int j=0;j<n;j++){
-                if(i==0 && j==0) dp[0][0]=grid[0][0];
+                if(i==0 && j==0) temp[j]=grid[0][0];
                 else{
-                    int up=INT_MAX;
+                    int up=dp[j];
                     int left=INT_MAX;
-                    if(valid(i-1,j,m,n)) up=dp[i-1][j];
-                    if(valid(i,j-1,m,n)) left=dp[i][j-1];
-
-                    dp[i][j]=grid[i][j]+min(up,left);
+                    if(j>0) left=temp[j-1];
+                    temp[j]=grid[i][j]+min(up,left);
                 }
             }
+            dp=temp;
         }
-        return dp[m-1][n-1];
+        return dp[n-1];
     }
 };
