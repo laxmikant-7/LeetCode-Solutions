fc4cd8133cd6026bb6a0b5172d1a9468af7e9d4e
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-08 17:34:32 +0530

    Time: 375 ms (26.7%), Space: 11.5 MB (92.74%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int t,int i){
-        if(t==0) return 1;
-        if(i==nums.size()-1){
-            return (t==nums[nums.size()-1])?1:0;
+    int countSubsets(vector<int>& nums, int i, int sum) {
+        if(i == nums.size()) {
+            return (sum == 0) ? 1 : 0;
         }
-        int take=0;
-        if(nums[i]<=t) take=solve(nums,t-nums[i],i+1);
-        int nontake=solve(nums,t,i+1);
-
-        return take+nontake;
+        int take = 0;
+        if(nums[i] <= sum)
+            take = countSubsets(nums, i + 1, sum - nums[i]);
+        int notTake = countSubsets(nums, i + 1, sum);
+        return take + notTake;
     }
+
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int totalsum=0;
-        for(int num:nums) totalsum+=num;
-        if((totalsum+target)%2) return 0;
-        int nt=(totalsum+target)/2;
-        return solve(nums,nt,0);
+        int totalSum = 0;
+        for(int x : nums) totalSum += x;
+        if(totalSum + target < 0 || (totalSum + target) % 2 != 0)
+            return 0;
+        int s1 = (totalSum + target) / 2;
+        return countSubsets(nums, 0, s1);
     }
 };
