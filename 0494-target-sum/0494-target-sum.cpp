accc6edee211657cc59c3a29ba70a39e944db136
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-08 17:21:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int i,int t,int c){
-        if(i==0){
-            int temp=0;
-            if(c+nums[0]==t) temp++;
-            if(c-nums[0]==t) temp++;
-            return temp;
+    int solve(vector<int>& nums,int t,int i){
+        if(t==0) return 1;
+        if(i==nums.size()-1){
+            return (t==nums[nums.size()-1])?1:0;
         }
-        int pos=solve(nums,i-1,t,c+nums[i]);
-        int neg=solve(nums,i-1,t,c-nums[i]);
+        int take=0;
+        if(nums[i]<=t) take=solve(nums,t-nums[i],i+1);
+        int nontake=solve(nums,t,i+1);
 
-        return pos+neg;
-        
-    
+        return take+nontake;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-        int count=0;
-        return solve(nums,n-1,target,count);
+        int totalsum=0;
+        for(int num:nums) totalsum+=num;
+        if((totalsum+target)%2) return 0;
+        int nt=(totalsum+target)/2;
+        return solve(nums,nt,0);
     }
 };
