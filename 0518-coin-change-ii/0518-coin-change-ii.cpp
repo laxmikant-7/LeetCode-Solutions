9c52af13f95ba57553755f6a82d185b50d06f918
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-09 19:26:24 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int solve(int a,vector<int>& coins,int i,vector<vector<int>> &dp){
-        if(a==0) return 1;
-        if(i==coins.size()-1) return (a%coins[i]==0)?1:0;
-        if(dp[i][a]!=-1) return dp[i][a];
-        int take=0;
-        if(coins[i]<=a) take=solve(a-coins[i],coins,i,dp);
-        int nontake=solve(a,coins,i+1,dp);
-        return dp[i][a]=take+nontake;
-    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         vector<vector<int>> dp(n,vector<int>(amount+1,-1));
-        return solve(amount,coins,0,dp);
+        for(int a=0;a<=amount;a++){
+            dp[0][a]=(a%coins[0]==0)?1:0;
+        }
+        for(int i=1;i<n;i++){
+            for(int a=0;a<=amount;a++){
+                int nontake=dp[i-1][a];
+                int take=0;
+                if(coins[i]<=a) take=dp[i][a-coins[i]];
+                dp[i][a]=take+nontake;
+            }
+        }
+    return dp[n-1][amount];
     }
 };
