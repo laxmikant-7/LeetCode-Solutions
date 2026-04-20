0490370a8ec2e8a3f79029cc2c5e245d55ef84ac
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-20 09:42:22 +0530

    Time: 0 ms (100%), Space: 12.1 MB (40.24%) - LeetHub

---
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int n=colors.size();
+        int ans=0;
+        for(int i=0;i<n-1;i++){
+            for(int j=n-1;j>i;j--){
+                if(colors[i]!=colors[j]){
+                    ans=max(ans,abs(i-j));
+                }
+            }
+        }
+        return ans;
+    }
+};
