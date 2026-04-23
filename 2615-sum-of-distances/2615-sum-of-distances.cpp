9e9d8a3bda7a77ef36aa39d97fa7f9d1929bc2ef
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-23 11:37:17 +0530

    Time: 72 ms (54.25%), Space: 112.8 MB (79.5%) - LeetHub

---
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<long long> distance(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,long long> tsum;
+        unordered_map<int,int> tcount;
+        vector<long long> ans(n);
+
+        vector<long long> presum(n);
+        vector<int> precount(n);
+        for(int i=0;i<n;i++){
+            presum[i]=(tsum.find(nums[i])!=tsum.end())?tsum[nums[i]]:1LL*0;
+            precount[i]=(tcount.find(nums[i])!=tcount.end())?tcount[nums[i]]:0;
+            tsum[nums[i]]+=1LL*i;
+            tcount[nums[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            int lcount=precount[i];
+            int rcount=tcount[nums[i]]-(lcount+1);
+            long long lsum=presum[i];
+            long long rsum=tsum[nums[i]]-(presum[i]+i);
+            ans[i]=abs((1LL*lcount*i)-lsum)+abs((1LL*rcount*i)-rsum);
+        }
+
+
+        
+        return ans;
+    }
+};
