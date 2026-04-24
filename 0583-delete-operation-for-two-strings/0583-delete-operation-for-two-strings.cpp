a7b48ae847a41b3f6cdbc0966451faf9388b7312
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-04-24 12:41:31 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int solve(string &s1, string &s2,int i,int j,vector<vector<int>> &dp){
-        if(i<0 || j<0){
+        if(i==0 || j==0){
             return 0;
         }
         if(dp[i][j]!=-1) return dp[i][j];
         int maxl=0;
-        if(s1[i]==s2[j]){
+        if(s1[i-1]==s2[j-1]){
             maxl=1+solve(s1,s2,i-1,j-1,dp);
         }
         else{
@@ -17,8 +17,8 @@ public:
     int minDistance(string word1, string word2) {
         int n1=word1.size();
         int n2=word2.size();
-        vector<vector<int>> dp(n1,vector<int>(n2,-1));
-        int maxlength=solve(word1,word2,n1-1,n2-1,dp);
+        vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
+        int maxlength=solve(word1,word2,n1,n2,dp);
         return (n1-maxlength)+(n2-maxlength);
     }
 };
