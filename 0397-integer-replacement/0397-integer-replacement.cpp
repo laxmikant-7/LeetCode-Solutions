61a70481069b40a7f0270bd6134e4c86315f6ec6
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-05-01 20:34:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.cpp
+++ 0397-integer-replacement/0397-integer-replacement.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
+    unordered_map<long long, int> dp;
     int solve(long long n){
-        if(n==1) return 0;
-        if(n%2==0){
-            return 1+solve(n/2);
+        if(n == 1) return 0;
+        if(dp.count(n)) return dp[n];
+
+        if(n % 2 == 0){
+            return dp[n] = 1 + solve(n / 2);
         }
-        return 1+min(solve(n+1),solve(n-1));
+        return dp[n] = 1 + min(solve(n + 1), solve(n - 1));
     }
+
     int integerReplacement(int n) {
-        return solve(1LL*n);
+        return solve((long long)n);
     }
 };
