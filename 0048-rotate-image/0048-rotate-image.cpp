cb2f755e399b147bd2cd626cecb46e4b4569649b
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-05-04 08:46:45 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -1,16 +1,15 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
-        int n=mat.size();
-        vector<vector<int>> temp(n,vector<int>(n));
-        int y=n-1;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                temp[j][y]=mat[i][j];
+        int n=mat[0].size();
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                swap(mat[i][j],mat[j][i]);
             }
-            y--;
         }
-        mat=temp;
+        for(int i=0;i<n;i++){
+            reverse(mat[i].begin(),mat[i].end());
+        }
         return;
     }
 };
