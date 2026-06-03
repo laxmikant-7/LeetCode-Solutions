f8dbdfcfc31fbf3bdbf86c6ee1ff1c56826d3ed9
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-06-03 07:44:37 +0530

    Time: 2 ms (4.76%), Space: 8.6 MB (33.76%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
+int solve(int n,vector<int> &dp){
+    if(n<=2) return n;
+    if(dp[n]!=-1) return dp[n];
+    return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+
+}
     int climbStairs(int n) {
-        if(n==1) return 1;
-        int prev2=1;
-        int prev1=2;
-        for(int i=3;i<=n;i++){
-            int curr=prev2+prev1;
-            prev2=prev1;
-            prev1=curr;
-        }
-        return prev1;
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
     }
 };
