0517c952cae5ed7ffb0d9d7dbc4d0654c3538ff0
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-06-06 13:16:27 +0530

    Time: 0 ms (100%), Space: 10.6 MB (60.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int find(int i,vector<int> &nums,vector<int> &dp){
-        if(i>=nums.size()) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int take=nums[i]+find(i+2,nums,dp);
-        int nontake=find(i+1,nums,dp);
-        return dp[i]=max(take,nontake);
-    }
+    // int find(int i,vector<int> &nums,vector<int> &dp){
+    //     if(i>=nums.size()-1) return 0;
+    //     if(i==nums.size()-1) return nums[i];
+    //     if(dp[i]!=-1) return dp[i];
+    //     int take=nums[i]+find(i+2,nums,dp);
+    //     int nontake=find(i+1,nums,dp);
+    //     return dp[i]=max(take,nontake);
+    // }
     int rob(vector<int>& nums) {
         int n=nums.size();
         vector<int> dp(n,-1);
-        return find(0,nums,dp);
+        // return find(0,nums,dp);
+        dp[0]=nums[0];
+        int neg=0;
+        for(int i=1;i<n;i++){
+            int take=nums[i];
+            if(i>1) take+=dp[i-2];
+            int nontake=dp[i-1];
+            dp[i]=max(take,nontake);
+        }
+        return dp[n-1];
     }
 };
