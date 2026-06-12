49aed4f2fc41d231fd7a8ad2ec848a827020ae20
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-06-12 23:08:45 +0530

    Time: 491 ms (25.56%), Space: 394.3 MB (25.56%) - LeetHub

---
+++ 3559-number-of-ways-to-assign-edge-weights-ii/3559-number-of-ways-to-assign-edge-weights-ii.cpp
@@ -0,0 +1,88 @@
+class Solution {
+public:
+    static const int MOD = 1e9 + 7;
+    static const int LOG = 17; // enough for 1e5
+
+    vector<vector<int>> adj;
+    vector<vector<int>> up;
+    vector<int> depth;
+    vector<long long> pow2;
+
+    void dfs(int node, int parent) {
+        up[node][0] = parent;
+
+        for (int j = 1; j < LOG; j++) {
+            up[node][j] = up[up[node][j - 1]][j - 1];
+        }
+
+        for (int nxt : adj[node]) {
+            if (nxt == parent) continue;
+            depth[nxt] = depth[node] + 1;
+            dfs(nxt, node);
+        }
+    }
+
+    int lca(int a, int b) {
+        if (depth[a] < depth[b])
+            swap(a, b);
+
+        int diff = depth[a] - depth[b];
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (diff & (1 << j))
+                a = up[a][j];
+        }
+
+        if (a == b) return a;
+
+        for (int j = LOG - 1; j >= 0; j--) {
+            if (up[a][j] != up[b][j]) {
+                a = up[a][j];
+                b = up[b][j];
+            }
+        }
+
+        return up[a][0];
+    }
+
+    vector<int> assignEdgeWeights(vector<vector<int>>& edges,
+                                  vector<vector<int>>& queries) {
+
+        int n = edges.size() + 1;
+
+        adj.assign(n + 1, {});
+        for (auto &e : edges) {
+            int u = e[0], v = e[1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        up.assign(n + 1, vector<int>(LOG));
+        depth.assign(n + 1, 0);
+
+        dfs(1, 0);
+
+        pow2.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++) {
+            pow2[i] = (pow2[i - 1] * 2) % MOD;
+        }
+
+        vector<int> ans;
+
+        for (auto &q : queries) {
+            int u = q[0];
+            int v = q[1];
+
+            int L = lca(u, v);
+
+            int len = depth[u] + depth[v] - 2 * depth[L];
+
+            if (len == 0)
+                ans.push_back(0);
+            else
+                ans.push_back(pow2[len - 1]);
+        }
+
+        return ans;
+    }
+};
