2dde55f80d2461a15aad6e5f25559bf451b8ce24
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-07-04 23:10:07 +0530

    Time: 3 ms (13.3%), Space: 18.4 MB (32.62%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,31 +3,29 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& i, int sr, int sc, int c) {
         int m=i.size();
         int n=i[0].size();
-        vector<vector<int>> temp=i;
-        temp[sr][sc]=c;
         vector<vector<bool>> vis(m,vector<bool>(n,false));
         vis[sr][sc]=true;
-        queue<pair<int,int>> q;
-        q.push({sr,sc});
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{sr,sc},i[sr][sc]});
+        i[sr][sc]=c;
         int dx[4]={-1,0,1,0};
         int dy[4]={0,1,0,-1};
         while(!q.empty()){
-            int x=q.front().first;
-            int y=q.front().second;
-            int val=i[x][y];
+            int x=q.front().first.first;
+            int y=q.front().first.second;
+            int val=q.front().second;
             q.pop();
-         
             for(int k=0;k<4;k++){
                 int nx=x+dx[k];
                 int ny=y+dy[k];
                 if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny] && i[nx][ny]==val){
-                    temp[nx][ny]=c;
+                    q.push({{nx,ny},val});
+                    i[nx][ny]=c;
                     vis[nx][ny]=true;
-                    q.push({nx,ny});
                 }
             }
 
         }
-        return temp;
+        return i;
     }
 };
