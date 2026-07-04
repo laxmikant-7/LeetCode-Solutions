fe17702cd85ace268acb4d059d14c90fdc1df4cd
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-07-04 22:57:38 +0530

    Time: 0 ms (100%), Space: 18.5 MB (21.14%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        int n=image.size();
-        int m=image[0].size();
-        int startCol=image[sr][sc];
-        if(startCol==color) return image;
+    vector<vector<int>> floodFill(vector<vector<int>>& i, int sr, int sc, int c) {
+        int m=i.size();
+        int n=i[0].size();
+        vector<vector<int>> temp=i;
+        temp[sr][sc]=c;
+        vector<vector<bool>> vis(m,vector<bool>(n,false));
+        vis[sr][sc]=true;
         queue<pair<int,int>> q;
         q.push({sr,sc});
-        image[sr][sc]=color;
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
+        int dx[4]={-1,0,1,0};
+        int dy[4]={0,1,0,-1};
         while(!q.empty()){
-            int r=q.front().first;
-            int c=q.front().second;
+            int x=q.front().first;
+            int y=q.front().second;
+            int val=i[x][y];
             q.pop();
-            image[r][c]=color;
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && image[nrow][ncol]==startCol){
-                    image[nrow][ncol]=color;
-                    q.push({nrow,ncol});
+         
+            for(int k=0;k<4;k++){
+                int nx=x+dx[k];
+                int ny=y+dy[k];
+                if(nx>=0 && nx<m && ny>=0 && ny<n && !vis[nx][ny] && i[nx][ny]==val){
+                    temp[nx][ny]=c;
+                    vis[nx][ny]=true;
+                    q.push({nx,ny});
                 }
             }
+
         }
-        return image;
+        return temp;
     }
 };
