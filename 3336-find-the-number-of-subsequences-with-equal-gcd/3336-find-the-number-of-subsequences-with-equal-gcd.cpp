ac185e5632aa837634d96ab4dcfa239013542234
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-07-14 11:51:23 +0530

    Time: 1440 ms (13.91%), Space: 292.2 MB (35.65%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -17,25 +17,26 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         int n=nums.size();
         int maxi=*max_element(nums.begin(),nums.end());
-        int dp[n+1][maxi+1][maxi+1];
-        memset(dp,0,sizeof(dp));
+        vector<vector<int>> prev(maxi+1,vector<int>(maxi+1,0));
         for(int seq1=0;seq1<=maxi;seq1++){
             for(int seq2=0;seq2<=maxi;seq2++){
-                dp[n][seq1][seq2]=(seq1!=0 && seq2!=0 && seq1==seq2)?1:0;
+                prev[seq1][seq2]=(seq1!=0 && seq2!=0 && seq1==seq2)?1:0;
             }
         }
         for(int i=n-1;i>=0;i--){
+            vector<vector<int>> curr(maxi+1,vector<int>(maxi+1,0));
             for(int seq1=0;seq1<=maxi;seq1++){
                 for(int seq2=0;seq2<=maxi;seq2++){
-                    int skip=dp[i+1][seq1][seq2];
-                    int take1=dp[i+1][gcd(seq1,nums[i])][seq2];
-                    int take2=dp[i+1][seq1][gcd(seq2,nums[i])];
-                    dp[i][seq1][seq2]=(0LL+skip+take1+take2)%M;
+                    int skip=prev[seq1][seq2];
+                    int take1=prev[gcd(seq1,nums[i])][seq2];
+                    int take2=prev[seq1][gcd(seq2,nums[i])];
+                    curr[seq1][seq2]=(0LL+skip+take1+take2)%M;
                 }
 
             }
+            swap(prev,curr);
 
         }
-        return dp[0][0][0];
+        return prev[0][0];
     }
 };
