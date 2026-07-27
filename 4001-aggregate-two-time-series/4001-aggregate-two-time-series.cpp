9e8a612bc7af49d150121e48764eec6517c2b37f
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-07-27 10:21:58 +0530

    Time: 89 ms (66.67%), Space: 350.2 MB (16.67%) - LeetHub

--- 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
+++ 4001-aggregate-two-time-series/4001-aggregate-two-time-series.cpp
@@ -5,41 +5,34 @@ public:
         int n2=s2.size();
         int i=0;
         int j=0;
-        unordered_map<int,int> m;
+        vector<vector<int>> temp;
         while(i<n1 && j<n2){
             int t1=s1[i][0];
             int v1=s1[i][1];
             int t2=s2[j][0];
             int v2=s2[j][1];
             if(t1==t2){
-                m[t1]+=(v1+v2);
+                temp.push_back({t1,v1+v2});
                 i++;
                 j++;
             }
             else if(t1<t2){
-                m[t1]+=(v1+v2);
+                temp.push_back({t1,v1+v2});
                 i++;
             }
             else{
-                m[t2]+=(v1+v2);
+                temp.push_back({t2,v1+v2});
                 j++;
             }
         }
         while(i<n1){
-            m[s1[i][0]]+=s1[i][1];
+            temp.push_back(s1[i]);
             i++;
         }
         while(j<n2){
-            m[s2[j][0]]+=s2[j][1];
+            temp.push_back(s2[j]);
             j++;
         }
-        vector<vector<int>> temp;
-        for(auto &it:m){
-            int time=it.first;
-            int val=it.second;
-            temp.push_back({time,val});
-        }
-        sort(temp.begin(),temp.end());
         return temp;
     }
 };
