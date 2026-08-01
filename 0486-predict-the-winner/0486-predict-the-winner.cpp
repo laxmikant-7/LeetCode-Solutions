2b10562f10560b1f1392cf5702675006fac3e9cc
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-08-01 10:18:17 +0530

    Time: 0 ms (100%), Space: 10.3 MB (16.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int find(vector<int>& nums,int i,int j){
+    int find(vector<int>& nums,int i,int j,vector<vector<int>> &dp){
         if(i==j) return nums[i];
         if(i>j) return 0;
-        int take_i=nums[i]+min(find(nums,i+2,j),find(nums,i+1,j-1));
-        int take_j=nums[j]+min(find(nums,i+1,j-1),find(nums,i,j-2));
-        return max(take_i,take_j);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take_i=nums[i]+min(find(nums,i+2,j,dp),find(nums,i+1,j-1,dp));
+        int take_j=nums[j]+min(find(nums,i+1,j-1,dp),find(nums,i,j-2,dp));
+        return dp[i][j]=max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
         int total_score=0;
+        vector<vector<int>> dp(n,vector<int>(n,-1));
         for(auto num:nums) total_score+=num;
-        int player1=find(nums,0,n-1);
+        int player1=find(nums,0,n-1,dp);
         int player2=total_score-player1;
         return player1>=player2;
     }
