1db3ed8465b035a286b1c66d5540c38eb8163ec2
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-08-20 09:45:00 +0530

    Time: 1 ms (7.4%), Space: 31 MB (21.62%) - LeetHub

---
+++ 4020-elevator-requests-i/4020-elevator-requests-i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int elevatorRequests(int n, vector<int>& requests) {
+        int last_floor=0;
+        int time=0;
+        for(int req:requests){
+            time+=abs(last_floor-req);
+            last_floor=req;
+        }
+        return time;
+    }
+};
