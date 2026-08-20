25849d71ff7ac1ecbb2791fa8fb658f8af8385a7
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-08-20 10:23:23 +0530

    Time: 155 ms (87.96%), Space: 10.7 MB (79.18%) - LeetHub

---
+++ 4021-minimum-operations-to-make-a-rotated-palindrome-i/4021-minimum-operations-to-make-a-rotated-palindrome-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        int op=INT_MAX;
+        int n=s.size();
+        for(int r=0;r<n;r++){
+            int i=0,j=n-1;
+            int temp=r;
+            while(i<=j){
+                temp+=min(abs(s[i] - s[j]), 26 - abs(s[i] - s[j]));
+                i++;
+                j--;
+            }
+            op=min(op,temp);
+            rotate(s.begin(), s.begin() + 1, s.end());
+        }
+        return op;
+    }
+};
