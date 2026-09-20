f0776d8788620af12310981e34546811ca9ee39f
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-09-20 09:18:55 +0530

    Time: 0 ms (100%), Space: 9.8 MB (15.78%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -4,8 +4,7 @@ public:
         int n=s.size();
         int rd=0;
         for(int i=0;i<n;i++){
-            int temp=26-(s[i]-'a');
-            temp*=(i+1);
+            int temp=(26-(s[i]-'a'))*(i+1);
             rd+=temp;
         }
         return rd;
