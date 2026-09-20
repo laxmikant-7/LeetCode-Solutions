cb0a37d8a32b2140febb9c41b99205615ea30446
Laxmikant Bantanur <laxmikantbantanur@gmail.com>
2026-09-20 09:18:30 +0530

    Time: 0 ms (100%), Space: 9.7 MB (47.88%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int reverseDegree(string s) {
+        int n=s.size();
+        int rd=0;
+        for(int i=0;i<n;i++){
+            int temp=26-(s[i]-'a');
+            temp*=(i+1);
+            rd+=temp;
+        }
+        return rd;
+    }
+};
